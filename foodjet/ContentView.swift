8209214458c5289815881dfab86f6961a4ba6610
Natdanai Chaiyakan <natdanai.c@geotalent.co.th>
2022-03-18 19:00:28 +0700

    Initial Commit

---
+++ foodjet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  foodjet
+//
+//  Created by Natdanai Chaiyakan on 18/3/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ foodjet/foodjetApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodjetApp.swift
+//  foodjet
+//
+//  Created by Natdanai Chaiyakan on 18/3/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct foodjetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
