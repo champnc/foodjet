f905c7a33ce2b4409ebec41bc87a16f61d8fc0f9
champnc <natdanai.dev@gmail.com>
2022-03-23 22:00:47 +0700

    decorate NotificationItem

--- foodjet/view/NotificationView.swift
+++ foodjet/view/NotificationView.swift
@@ -9,19 +9,33 @@ import SwiftUI
 
 struct NotificationView: View {
     var body: some View {
-        List() {
-            ForEach(0..<8) { _ in
-                HStack {
-                    NotificationItem()
+        NavigationView {
+            List() {
+                ForEach(0..<8) { _ in
+                    HStack {
+                        NotificationItem()
+                    }
                 }
-            }
+            }.navigationTitle(Text("Notification")).listStyle(InsetGroupedListStyle())
         }
     }
 }
 
 struct NotificationItem: View {
     var body: some View {
-        Text("Noti").frame(height: 50)
+        HStack(spacing : 20) {
+            VStack(alignment: .leading, spacing: -20) {
+                HStack {
+                    Text("Notification").bold().frame(height: 50)
+                    Spacer()
+                    Text("10:00").frame(height: 50)
+                }
+                HStack(alignment: .lastTextBaseline) {
+                    Text("Notification").frame(height: 50).font(.caption2)
+                }
+            }
+            Circle().fill(Color.orange).frame(width: 10, height: 10)
+        }
     }
 }
 
