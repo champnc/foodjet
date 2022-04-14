6f3c6875a0af193d9321167c3550f91f9ef7865a
champnc <natdanai.dev@gmail.com>
2022-04-14 20:56:33 +0700

    decorate detail view

--- foodjet/view/DetailView.swift
+++ foodjet/view/DetailView.swift
@@ -9,28 +9,21 @@ import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        NavigationView {
             ScrollView(.vertical) {
                 VStack {
-                    ScrollView(.horizontal) {
-                        HStack {
-                            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                            Text("Hello, World!")
-                            Text("Hello, World!")
-                        }
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20) {
+                            MenuOverviewItemView()
+                            MenuOverviewItemView()
+                            MenuOverviewItemView()
+                        }.padding()
                     }
-                    VStack {
-                        HStack{
-                            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).font(.title2).bold()
-                            Spacer()
-                        }
-                        var list = ["1","2","3"]
-                        ForEach(list) { item in
-                            Text("\(item) List Item")
-                        }
-                        
-                        
-                    }.padding().background()
+                    MenuSectionView()
+                    Divider()
+                    MenuSectionView()
+                    Divider()
+                    MenuSectionView()
+                    Divider()
 //                    VStack {
 //                        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
 //                    }
@@ -40,14 +33,55 @@ struct DetailView: View {
 //                    VStack {
 //                        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
 //                    }
-                }
+                }.navigationBarTitle("Menu Name")
             }
-        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView()
+    }
+}
+
+struct MenuItem : Identifiable , Decodable {
+    var id = UUID()
+    var name : String
+    var price : Double
+}
+
+struct MenuSectionView: View {
+    let list = [MenuItem(name: "menu1", price: 10.0),
+                MenuItem(name: "menu2", price: 20),
+                MenuItem(name: "menu3", price: 30)]
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack{
+                Text("Menu Section").font(.title2).bold()
+                Spacer()
+            }
+            ForEach(list) { item in
+                HStack {
+                    Text("image").frame(width: 75, height: 75).foregroundColor(.black).background(.yellow).cornerRadius(5).shadow(color: .gray, radius: 2, x: 2, y: 2)
+                    VStack(alignment: .leading) {
+                        Text("\(item.name) Item")
+                        Text("\(item.price.formatted()) ฿").bold()
+                    }.padding(.leading)
+                    Spacer()
+                    Image(systemName: "plus").foregroundColor(.orange)
+                }
+                Divider()
+            }
+            
+            
+        }.padding().background()
+    }
+}
+
+struct MenuOverviewItemView: View {
+    var body: some View {
+        ZStack {
+            Text("Item").foregroundColor(Color.black).frame(width: 300, height: 250).background(Color.yellow).cornerRadius(15).shadow(color: Color.gray, radius: 2, x: 2, y: 2)
+        }
     }
 }
