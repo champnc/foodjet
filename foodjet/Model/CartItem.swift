6e7fe7fd2244a8d4a296458cd063253b3209be4f
champnc <natdanai.dev@gmail.com>
2022-04-23 21:49:53 +0700

    add mock cart item

---
+++ foodjet/Model/CartItem.swift
@@ -0,0 +1,16 @@
+//
+//  CartItem.swift
+//  foodjet
+//
+//  Created by Natdanai Chaiyakan on 23/4/2565 BE.
+//
+
+import Foundation
+
+struct CartItem : Identifiable, Decodable {
+    var id : Int
+    var title : String
+//    var description : String
+    var time : String
+//    var type : Int
+}
