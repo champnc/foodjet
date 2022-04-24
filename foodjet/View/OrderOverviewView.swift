dd3de92a8aa1db1587030227e213bc584df12e2f
champnc <natdanai.dev@gmail.com>
2022-04-24 19:56:55 +0700

    add overviewView

---
+++ foodjet/View/OrderOverviewView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderOverviewView.swift
+//  foodjet
+//
+//  Created by Natdanai Chaiyakan on 24/4/2565 BE.
+//
+
+import SwiftUI
+
+struct OrderOverviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderOverviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderOverviewView()
+    }
+}
