923b120a212acff1e32400b1fac7e28e08e274f1
champnc <natdanai.dev@gmail.com>
2022-06-11 09:40:00 +0700

    decorate overview

--- foodjet/View/OrderOverviewView.swift
+++ foodjet/View/OrderOverviewView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct OrderOverviewView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack {
+                ScrollView(.vertical) {
+                    HStack{
+                        Text("Overview").font(.title2).bold()
+                        Spacer()
+                    }.padding()
+                }
+                VStack {
+                    HStack{
+                        Text("Total:").bold()
+                        Spacer()
+                        Text("100").bold()
+                    }.padding(.horizontal,20)
+                    Button {
+                        
+                    } label: {
+                        HStack(alignment: .center, spacing: 10) {
+                            Text("Place Order")
+                                .bold()
+                                .foregroundColor(Color.white)
+                        }
+                    }.frame(width: UIScreen.main.bounds.size.width - 40, height: 50)
+                        .background(Color.orangeLight)
+                        .cornerRadius(15)
+                        .padding(.horizontal, 20).padding(.bottom, 20)
+                }
+            }.navigationBarTitle("", displayMode: .inline)
     }
 }
 
